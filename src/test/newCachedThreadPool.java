13d25df160f369bd03fc12da0051c4c63e48c84c
落落酱 <574490800@qq.com>
2017-11-02 09:27:08 +0800

    创建一个可缓存线程池，如果线程池长度超过处理需要，可灵活回收空闲线程，若无可回收，则新建线程。

---
+++ src/test/newCachedThreadPool.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by LYQ on 2017/11/2 0002.
+ */
+public class newCachedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            try {
+                Thread.sleep(index * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            cachedThreadPool.execute(new Runnable() {
+                public void run() {
+                    System.out.println(index);
+                }
+            });
+        }
+    }
+}
