d551b818ab5447d5ffc8945e9821fa6984d29ccc
落落酱 <574490800@qq.com>
2017-11-02 09:35:10 +0800

    创建一个定长线程池，可控制线程最大并发数，超出的线程会在队列中等待

---
+++ src/test/newFixedThreadPool.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by LYQ on 2017/11/2 0002.
+ */
+public class newFixedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            fixedThreadPool.execute(new Runnable() {
+                public void run() {
+                    try {
+                        System.out.println(index);
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
